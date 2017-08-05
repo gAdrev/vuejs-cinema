974662018a5c579511aac9a84ab534c126963d96
Javier V <javiervilla00@gmail.com>
2017-08-06 00:40:09 +0200

    Detail page (lec 108)

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,7 +1,34 @@
 <template>
     <div id="detail" v-if="movie">
-        <movie-item v-bind:movie="movie.movie">
+        <movie-item v-bind:movie="movie">
+            <div class="movie-details">
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.Plot }}</p>
+                <table>
+                    <tr>
+                        <td>Released date:</td>
+                        <td>{{ movie.Released }}</td>
+                    </tr>
+                    <tr>
+                        <td>Running time:</td>
+                        <td>{{ movie.Runtime }}</td>
+                    </tr>
+                    <tr>
+                        <td>Director:</td>
+                        <td>{{ movie.Director }}</td>
+                    </tr>
+                    <tr>
+                        <td>Cast:</td>
+                        <td>{{ movie.Actors }}</td>
+                    </tr>
+                </table>
+            </div>
         </movie-item>
+        <div class="home">
+            <router-link v-bind:to="{ name: 'home' }">
+                Back to results
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -13,7 +40,7 @@ export default {
         movie: function movie() {
             const routeId = this.$route.params.id,
                   movie = this.movies.find(movie => movie.id === routeId);
-            return movie ? movie : null;
+            return movie ? movie.movie : null;
         }
     },
     created: function created() {
