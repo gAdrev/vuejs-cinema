5773e02e0dd5731460e7bc2916e708690db6354b
Javier V <javiervilla00@gmail.com>
2017-08-07 23:54:41 +0200

    Day selection: apply styles to selected date (lec 111)

--- src/components/DaySelect.vue
+++ src/components/DaySelect.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="day-select">
-      <ul class="days"> <li class="day" v-for="day in days">{{ formatDay(day) }}</li> </ul>
+      <ul class="days">
+          <li v-bind:class="{day: true, active: isActive(day)}" v-for="day in days" v-on:click="selected = day">
+          {{ formatDay(day) }}
+          </li>
+      </ul>
   </div>
 </template>
 <<script>
 export default {
   data() {
       return {
+          selected: this.$moment(),
           days: [0, 1, 2, 3, 4, 5, 6].map(num => this.$moment().add(num, "days"))
       };
   },
@@ -16,6 +21,9 @@ export default {
               return "Today";
           }
           return raw.format("ddd DD/MM");
+      },
+      isActive(day) {
+          return day.isSame(this.selected, "day");
       }
   }
 }
